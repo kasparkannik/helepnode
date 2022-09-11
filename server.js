7ec6014a07c2fa7a6805954a7a14451895d83419
Henri <Henri@Henrisponalcomp.lan>
2022-09-11 21:43:07 +0300

    first push

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/add", (req, res) => {
+  const { a } = req.body;
+  res.send({
+    result: String(a)
+  });
+});
+
+app.listen(2000, () => {
+  console.log(`Server is running on port 2000.`);
+});
