b11fd90204da661142c248c3d8b7c21b23ccbe8d
Henri <Henri@Henrisponalcomp.lan>
2022-09-12 13:23:20 +0300

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-const server = app.listen(process.env.PORT || 5000, () => {
-  const port = server.address().port;
-  console.log(`Express is working on port ${port}`);
-});
+var PORT = process.env.PORT || 5000;
 
 const path = require("path");
 const express = require("express");
